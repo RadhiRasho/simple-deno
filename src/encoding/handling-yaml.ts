cc2591f3c624b0e3839340e1015a9295c13dd997
Radhi Rasho <Radhi.h@outlook.com>
2024-10-29 20:03:08 +0000

    json and yaml

---
+++ src/encoding/handling-yaml.ts
@@ -0,0 +1,32 @@
+import { parse, stringify } from "@std/yaml";
+
+const text = `
+foo: bar
+baz:
+  - qux
+  - quux
+`;
+
+interface ParsedData {
+	foo?: string;
+	baz?: string[];
+}
+
+const data = parse(text) as ParsedData;
+
+console.log(data.foo);
+console.log(data.baz?.length);
+
+const obj = {
+	hello: "world",
+	numbers: [1, 2, 3],
+};
+
+const yaml = stringify(obj);
+
+console.log(yaml);
+// hello: word
+// numbers:
+//   - 1
+//   - 2
+//   - 3
