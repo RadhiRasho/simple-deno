968fe38642f1397524ffdfee50370d2bf40c11f5
Radhi Rasho <Radhi.h@outlook.com>
2024-10-31 14:54:06 +0000

    test

---
+++ src/cli/writing-tests.ts
@@ -0,0 +1,42 @@
+import { assert, assertEquals } from "@std/assert";
+
+Deno.test("assert works correclty", () => {
+	assert(true);
+	assertEquals(1, 1);
+});
+
+Deno.test("testing steps", async (t) => {
+	const file = await Deno.open("example.txt", {
+		read: true,
+		write: true,
+		create: true,
+	});
+
+	const encoder = new TextEncoder();
+
+	const data = encoder.encode("Hello World");
+
+	await t.step("write some bytes", async () => {
+		const bytesWritten = await file.write(data);
+		assertEquals(bytesWritten, data.length);
+		await file.seek(0, Deno.SeekMode.Start);
+	});
+
+	await t.step("Write some bytes", async () => {
+		const buffer = new Uint8Array(data.length);
+
+		await file.read(buffer);
+
+		assertEquals(buffer, data);
+	});
+
+	file.close();
+});
+
+Deno.test({
+	name: "Leaky Test",
+	async fn() {
+		await Deno.open("example.txt");
+	},
+	sanitizeResources: false,
+});
