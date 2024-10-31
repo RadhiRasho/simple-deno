e6837b79346de420d1e761c1424c701ec4e8a3df
Radhi Rasho <Radhi.h@outlook.com>
2024-10-31 20:07:52 +0000

    deno version + netwokring

---
+++ src/cli/deno-version.ts
@@ -0,0 +1,5 @@
+console.log("Current Deno Version: ", Deno.version.deno);
+
+console.log("Current Typescript Version: ", Deno.version.typescript);
+
+console.log("Current V8 Version: ", Deno.version.v8);
