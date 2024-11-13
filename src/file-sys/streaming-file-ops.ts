c1913d1932a5db02559b8dbedfd69c8b01c2d41a
Radhi Rasho <Radhi.h@outlook.com>
2024-11-13 19:37:20 +0000

    streaming file operations

---
+++ src/file-sys/streaming-file-ops.ts
@@ -0,0 +1,30 @@
+import { sleep } from "../utils/index.ts";
+
+const output = await Deno.open("example.txt", { create: true, append: true });
+
+const outputWriter = output.writable.getWriter();
+
+await outputWriter.ready;
+
+const outputText = "I love Deno!\n";
+
+const encoded = new TextEncoder().encode(outputText);
+
+await sleep(2000);
+
+await outputWriter.write(encoded);
+
+await outputWriter.close();
+
+const input = await Deno.open("example.txt");
+
+const inputReader = input.readable.getReader();
+
+const decoder = new TextDecoder();
+
+while (true) {
+	const result = await inputReader.read();
+
+	if (result.done) break;
+	console.log(`Read chunk: ${decoder.decode(result.value)}`);
+}
