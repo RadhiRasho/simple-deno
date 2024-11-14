abe720d40177e4b3c4cbc60ed5128c6e07019a14
Radhi Rasho <Radhi.h@outlook.com>
2024-11-14 21:19:02 +0000

    exponential backoff

---
+++ src/advanced/exponential-backoff.ts
@@ -0,0 +1,22 @@
+import { RetryError, type RetryOptions, retry } from "@std/async";
+
+function fn() {
+	console.log("hello world");
+	return Promise.reject("Rejected");
+}
+
+const options: RetryOptions = {
+	maxAttempts: 3,
+	minTimeout: 10,
+	multiplier: 2,
+	jitter: 0,
+};
+
+try {
+	await retry(fn, options);
+} catch (err) {
+	if (err instanceof RetryError) {
+		console.log("Retry error :", err.message);
+		console.log("Error cause :", err.cause);
+	}
+}
