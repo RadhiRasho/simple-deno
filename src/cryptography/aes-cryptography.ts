2efd88d9535a257b7577183c55f63517d60f43d1
Radhi Rasho <Radhi.h@outlook.com>
2024-11-14 21:12:03 +0000

    hashing and aes encryption and decryption

---
+++ src/cryptography/aes-cryptography.ts
@@ -0,0 +1,39 @@
+import { crypto } from "@std/crypto";
+
+const text = "Hello, Deno 2.0!";
+
+const encoder = new TextEncoder();
+
+const data = encoder.encode(text);
+
+const key = await crypto.subtle.generateKey(
+	{ name: "AES-GCM", length: 256 }, // 256-bit encryption key for strong security
+	true, // The key is extractable for encryption and decryption
+	["encrypt", "decrypt"], // Key usages: encryption and decryption
+);
+
+const iv = crypto.getRandomValues(new Uint8Array(12)); // 12-byte IV for AES-GCM
+
+const encryptedData = await crypto.subtle.encrypt(
+	{
+		name: "AES-GCM",
+		iv,
+	},
+	key,
+	data,
+);
+
+console.log(`Encrypted Data: ${new Uint8Array(encryptedData)}`); // Log the encrypted result as a byte array
+
+const decryptedData = await crypto.subtle.decrypt(
+	{
+		name: "AES-GCM",
+		iv,
+	},
+	key,
+	encryptedData,
+);
+
+const decryptedText = new TextDecoder().decode(decryptedData);
+
+console.log(`Decrypted Text: ${decryptedText}`);
--- src/cryptography/hashing.ts
+++ src/cryptography/hashing.ts
@@ -1,13 +1,21 @@
 import { crypto } from "@std/crypto";
 
+import { encodeHex } from "@std/encoding/hex";
+
 const message = "The easiest, most secure JavaScript runtime";
 
 const messageBuffer = new TextEncoder().encode(message);
 
 const hashBuffer = await crypto.subtle.digest("SHA-256", messageBuffer);
 
-import { encodeHex } from "@std/encoding/hex";
-
 const hash = encodeHex(hashBuffer);
 
 console.log(hash);
+
+const fileContents = await Deno.readFile("./example.txt");
+
+const fileHashBuffer = await crypto.subtle.digest("SHA-256", fileContents);
+
+const fileHash = encodeHex(new Uint8Array(fileHashBuffer));
+
+console.log(fileHash);
